4bce7db77fea4caeb52c530e059fe49910d35eb9
devaraujo1 <dev.muriloaraujo@gmail.com>
2026-02-23 19:45:01 -0300

    Update seguranca_pos_venda_database.sql

--- Database/05_seguranca_pos_venda/seguranca_pos_venda_database.sql
+++ Database/05_seguranca_pos_venda/seguranca_pos_venda_database.sql
@@ -1,2 +1,10 @@
 -- Módulo 05 – Segurança e Pós-venda
 -- SGBD: PostgreSQL
+
+
+CREATE TABLE IF NOT EXISTS tb_reembolsos_revogacoes (
+    id_reembolso SERIAL PRIMARY KEY,
+    id_pedido INT NOT NULL,
+    motivo TEXT,
+    status_reembolso VARCHAR(20) NOT NULL CHECK (status_reembolso IN ('Aprovado', 'Negado', 'Pendente'))
+);
