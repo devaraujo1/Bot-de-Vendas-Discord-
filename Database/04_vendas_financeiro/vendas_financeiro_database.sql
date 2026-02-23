ba9c9ee610b2fef99ebd3a515cf9a98a8b95605b
devaraujo1 <dev.muriloaraujo@gmail.com>
2026-02-23 20:55:42 -0300

    Update vendas_financeiro_database.sql

--- Database/04_vendas_financeiro/vendas_financeiro_database.sql
+++ Database/04_vendas_financeiro/vendas_financeiro_database.sql
@@ -1,7 +1,7 @@
 -- Módulo 04 – Vendas
 -- SGBD: PostgreSQL
 
-
+-- Tabela: tb_cupons
 CREATE TABLE IF NOT EXISTS tb_cupons (
     id_cupom SERIAL PRIMARY KEY,
     codigo VARCHAR(50) NOT NULL UNIQUE,
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS tb_cupons (
     data_validade DATE
 );
 
+--Tabela: tb_pedidos
 CREATE TABLE IF NOT EXISTS tb_pedidos (
     id_pedido SERIAL PRIMARY KEY,
     id_comprador_discord BIGINT NOT NULL,
@@ -17,10 +18,22 @@ CREATE TABLE IF NOT EXISTS tb_pedidos (
     status_pagamento VARCHAR(20) NOT NULL CHECK (status_pagamento IN ('Pendente', 'Pago', 'Cancelado'))
 );
 
-
+--Tabela: tb_itens_pedido
 CREATE TABLE IF NOT EXISTS tb_itens_pedido (
     id_item_pedido SERIAL PRIMARY KEY,
     id_pedido INT NOT NULL,
     id_produto INT NOT NULL,
     quantidade INT NOT NULL CHECK (quantidade > 0)
+);
+
+
+--Tabela: tb_pagamentos_pix
+CREATE TABLE IF NOT EXISTS tb_pagamentos_pix (
+    id_pagamento SERIAL PRIMARY KEY,
+    id_pedido INT NOT NULL UNIQUE,
+    codigo_copia_cola TEXT NOT NULL,
+    confirmado BOOLEAN DEFAULT FALSE,
+    txid VARCHAR(100) UNIQUE,
+    expiracao_pagamento TIMESTAMP,
+    id_cargo_recompensa INT
 );
