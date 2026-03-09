a866f5b9dae8c6e7879423012729a338f8061d9b
João Paulo Carneiro <jotapecno@gmail.com>
2026-03-09 02:54:53 -0300

    Remoção dos "tb" tickets_atendimento_database.sql

--- Database/03_tickets_atendimento/tickets_atendimento_database.sql
+++ Database/03_tickets_atendimento/tickets_atendimento_database.sql
@@ -1,10 +1,10 @@
-CREATE TABLE tb_tipos_ticket (
+CREATE TABLE tipos_ticket (
     id_tipo SERIAL PRIMARY KEY,
     nome_tipo VARCHAR(100) NOT NULL,
     descricao VARCHAR(255)
 );
 
-CREATE TABLE tb_tickets (
+CREATE TABLE tickets (
     id_ticket SERIAL PRIMARY KEY,
     id_canal_discord BIGINT NOT NULL,
     id_dono_discord BIGINT NOT NULL,
@@ -15,10 +15,10 @@ CREATE TABLE tb_tickets (
     
     CONSTRAINT fk_tipo
         FOREIGN KEY (id_tipo)
-        REFERENCES tb_tipos_ticket(id_tipo)
+        REFERENCES tipos_ticket(id_tipo)
 );
 
-CREATE TABLE tb_mensagens_ticket (
+CREATE TABLE mensagens_ticket (
     id_mensagem SERIAL PRIMARY KEY,
     id_ticket INT NOT NULL,
     id_autor_discord BIGINT NOT NULL,
@@ -27,10 +27,10 @@ CREATE TABLE tb_mensagens_ticket (
     
     CONSTRAINT fk_ticket
         FOREIGN KEY (id_ticket)
-        REFERENCES tb_tickets(id_ticket)
+        REFERENCES tickets(id_ticket)
 );
 
-CREATE TABLE tb_avaliacoes_atendimento (
+CREATE TABLE avaliacoes_atendimento (
     id_avaliacao SERIAL PRIMARY KEY,
     id_ticket INT NOT NULL,
     nota INT CHECK (nota BETWEEN 1 AND 5),
@@ -38,5 +38,5 @@ CREATE TABLE tb_avaliacoes_atendimento (
     
     CONSTRAINT fk_ticket_avaliacao
         FOREIGN KEY (id_ticket)
-        REFERENCES tb_tickets(id_ticket)
+        REFERENCES tickets(id_ticket)
 );
